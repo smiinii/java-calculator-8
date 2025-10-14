fce624d772ae7d72a5ed7964b2706ffda36aba3c
smini <sm020320@naver.com>
2025-10-14 22:32:14 +0900

    refactor(view): 입력 null 처리 검증 추가

--- src/main/java/calculator/view/IO.java
+++ src/main/java/calculator/view/IO.java
@@ -5,7 +5,11 @@ import camp.nextstep.edu.missionutils.Console;
 public class IO {
 
     public String input() {
-        return Console.readLine();
+        String inputs = Console.readLine();
+        if (inputs == null) {
+            throw new IllegalArgumentException("입력을 읽을 수 없습니다.");
+        }
+        return inputs;
     }
 
     public void printResult(int result) {
