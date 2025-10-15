ebe2180a302abbabb5180b92cb67a1a7e7030988
smini <sm020320@naver.com>
2025-10-15 23:34:27 +0900

    feat(application): 입출력 흐름 통합 및 실행 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,17 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.domain.Delimiter;
+import calculator.view.IO;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        IO io = new IO();
+        Delimiter delimiter = new Delimiter();
+        Calculator calculator = new Calculator(delimiter);
+
+        String input = io.input();
+        int result = calculator.calculate(input);
+        io.printResult(result);
     }
 }
