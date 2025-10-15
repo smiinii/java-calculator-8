68f023eac057270b53100bc58fa4eca90728176d
smini <sm020320@naver.com>
2025-10-15 22:27:19 +0900

    feat(delimiter):  커스텀 구분자 처리 구현 + 테스트
    - "//<구분자>\\n" 형식의 입력에서 구분자 파싱 및 검증 로직 구현
    - 커스텀 구분자의 형식, 길이 및 공백, 숫자 사용 불가 조건 검증
    - 숫자부분이 비어있는 경우 예외 처리
    - CustomDelimiterTest 작성 및 예외 케이스 테스트 완료

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -2,35 +2,79 @@ package calculator.domain;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Delimiter {
 
-    private static final String DEFAULT_DELIMITER = "[,:]";
-    private static final String VALIDATE_DEFAULT = ".*[^0-9,:].*";
+    private static final String BASE_DELIMS = ",:";
+    private static final String DEFAULT_DELIMITER = "[" + BASE_DELIMS + "]";
+    private static final String VALIDATE_DEFAULT = ".*[^0-9" + BASE_DELIMS + "].*";
+    private static final String CUSTOM_DELIMITER_START = "//";
+    private static final String CUSTOM_DELIMITER_END = "\\n";
+    private static final int CUSTOM_DELIMITER_START_INDEX = 2;
 
     public List<String> tokenize(String inputs) {
         if (isDefault(inputs)) {
             return defaultTokenize(inputs);
         }
-        throw new UnsupportedOperationException("커스텀 구분자 아직 미구현");
-    }
-
-    private List<String> defaultTokenize(String inputs) {
-        validateDefaultOnly(inputs);
-        return Arrays.stream(inputs.split(DEFAULT_DELIMITER)).toList();
+        return customTokenize(inputs);
     }
 
     private boolean isDefault(String inputs) {
         return !inputs.startsWith("//");
     }
 
-    private void validateDefaultOnly(String inputs) {
+    private List<String> defaultTokenize(String inputs) {
         if (inputs.matches(VALIDATE_DEFAULT)) {
             throw new IllegalArgumentException("기본 구분자는 쉼표(,) 또는 콜론(:)만 허용됩니다.");
         }
+        return Arrays.stream(inputs.split(DEFAULT_DELIMITER)).toList();
     }
 
+    private List<String> customTokenize(String inputs) {
+        int lnIndex = validateCustomFormat(inputs);
+        char customToken = validateCustomDelimiter(inputs, lnIndex);
+        String numbersPart = validateNumberPart(inputs, lnIndex);
+        String normalized =  normalizeDelimiters(numbersPart, customToken);
 
+        if (normalized.matches(VALIDATE_DEFAULT)) {
+            throw new IllegalArgumentException("기본 구분자(쉼표(,) 또는 콜론(:))와 커스텀 구분자(" + customToken +")만 허용됩니다.");
+        }
+        return Arrays.stream(normalized.split(DEFAULT_DELIMITER)).toList();
+    }
 
+    private int validateCustomFormat(String inputs) {
+        if (!inputs.startsWith(CUSTOM_DELIMITER_START)) {
+            throw new IllegalArgumentException("커스텀 형식은 '//'으로 시작해야 합니다.");
+        }
+        int lnIndex = inputs.indexOf(CUSTOM_DELIMITER_END);
+        if (lnIndex < 0) {
+            throw new IllegalArgumentException("커스텀 형식은 '\n'이 존재해야 합니다.");
+        }
+        return lnIndex;
+    }
+
+    private char validateCustomDelimiter(String inputs, int lnIndex) {
+        String customDelimiter = inputs.substring(CUSTOM_DELIMITER_START_INDEX, lnIndex);
+        if (customDelimiter.length() != 1) {
+            throw new IllegalArgumentException("커스텀 구분자는 한 글자만 허용됩니다.");
+        }
+        char customToken = customDelimiter.charAt(0);
+        if (Character.isWhitespace(customToken) || Character.isDigit(customToken)) {
+            throw new IllegalArgumentException("커스텀 구분자로 공백과 숫자는 허용되지 않습니다.");
+        }
+        return customToken;
+    }
+
+    private String validateNumberPart(String inputs, int lnIndex) {
+        int startNumberPart = lnIndex + CUSTOM_DELIMITER_END.length();
+        if (startNumberPart == inputs.length()) {
+            throw new IllegalArgumentException("숫자 부분이 비어있습니다.");
+        }
+        return inputs.substring(startNumberPart);
+    }
+
+    private String normalizeDelimiters(String numbersPart, char customToken) {
+        char defaultDelimiter = BASE_DELIMS.charAt(0);
+        return numbersPart.replace(customToken, defaultDelimiter);
+    }
 }
---
+++ src/test/java/calculator/CustomDelimiterTest.java
@@ -0,0 +1,64 @@
+package calculator;
+
+import calculator.domain.Delimiter;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class CustomDelimiterTest {
+    @Test
+    void 커스텀_구분자_분리() {
+        Delimiter d = new Delimiter();
+        assertThat(d.tokenize("//;\\n1,2;3")).containsExactly("1","2","3");
+    }
+
+    @Test
+    void 커스텀_구분자_형식이_다르면_예외() {
+        Delimiter d = new Delimiter();
+        assertThatThrownBy(() -> d.tokenize("/;\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> d.tokenize("//;\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> d.tokenize("//;\t1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> d.tokenize(";\t1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> d.tokenize("//;1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀_구분자_한글자_넘어가면_예외() {
+        Delimiter d = new Delimiter();
+        assertThatThrownBy(() -> d.tokenize("//;[\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> d.tokenize("//;;\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> d.tokenize("//.;\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀_구분자_공백_또는_숫자시_예외() {
+        Delimiter d = new Delimiter();
+        assertThatThrownBy(() -> d.tokenize("//\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> d.tokenize("//5\\n1,2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 커스텀_구분자_또는_기본_구분자가_아닐시_예외() {
+        Delimiter d = new Delimiter();
+        assertThatThrownBy(() -> d.tokenize("//;\\n1,2;3.4"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_부분이_비어있으면_예외() {
+        Delimiter d = new Delimiter();
+        assertThatThrownBy(() -> d.tokenize("//;\\n"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
