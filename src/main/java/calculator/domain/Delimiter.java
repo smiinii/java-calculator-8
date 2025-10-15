ed70927d62f1dfe8066010334f4fb0000d3561c6
smini <sm020320@naver.com>
2025-10-15 12:38:45 +0900

    feat(Delimiter): 기본 구분자 분리 및 검증 구현 + 테스트
    - 입력을 ", :" 기준으로 분리
    - 허용되지 않는 문자 및 구분자 입력시 IllegalArgumentException 발생
    - 기본/무구분자/금지문자 케이스 테스트 추가 및 통과

---
+++ src/main/java/calculator/domain/Delimiter.java
@@ -0,0 +1,39 @@
+package calculator.domain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Delimiter {
+
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String VALIDATE_DEFAULT = ".*[^0-9,:].*";
+
+    public List<String> tokenize(String inputs) {
+        if (isDefault(inputs)) {
+            return defaultTokenize(inputs);
+        }
+        throw new UnsupportedOperationException("커스텀 구분자 아직 미구현");
+    }
+
+    private List<String> defaultTokenize(String inputs) {
+        validateDefaultOnly(inputs);
+        return Arrays.stream(inputs.split(DEFAULT_DELIMITER))
+                .map(String::trim)
+                .filter(s -> !s.isEmpty())
+                .toList();
+    }
+
+    private boolean isDefault(String inputs) {
+        return !inputs.startsWith("//");
+    }
+
+    private void validateDefaultOnly(String inputs) {
+        if (inputs.matches(VALIDATE_DEFAULT)) {
+            throw new IllegalArgumentException("기본 구분자는 쉼표(,) 또는 콜론(:)만 허용됩니다.");
+        }
+    }
+
+
+
+}
---
+++ src/test/java/calculator/DelimiterTest.java
@@ -0,0 +1,42 @@
+package calculator;
+
+import calculator.domain.Delimiter;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class DelimiterTest {
+
+    @Test
+    void 기본_구분자로_분리() {
+        Delimiter defaultDelimiter = new Delimiter();
+        List<String> tokens = defaultDelimiter.tokenize("1,2:3");
+        assertThat(tokens).containsExactly("1", "2", "3");
+    }
+
+    @Test
+    void 구분자_없으면_그대로_반환() {
+        Delimiter defaultDelimiter = new Delimiter();
+        List<String> tokens = defaultDelimiter.tokenize("123");
+        assertThat(tokens).containsExactly("123");
+    }
+
+    @Test
+    void 기본_구분자가_아니면_예외() {
+        Delimiter defaultDelimiter = new Delimiter();
+        assertThatThrownBy(() -> defaultDelimiter.tokenize("1;2;3"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 아무_문자열_입력시_예외() {
+        Delimiter defaultDelimiter = new Delimiter();
+        assertThatThrownBy(() -> defaultDelimiter.tokenize("fjdifre"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
