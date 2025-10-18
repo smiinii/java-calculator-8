feb177de2deb525e984f12f2ee19d2639bb41d94
smini <sm020320@naver.com>
2025-10-18 12:11:48 +0900

    refactor(IO): 입력 안내 메시지 추가

--- src/main/java/calculator/view/IO.java
+++ src/main/java/calculator/view/IO.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class IO {
 
     public String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputs = Console.readLine();
         if (inputs == null) {
             throw new IllegalArgumentException("입력을 읽을 수 없습니다.");
